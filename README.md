8a5ba939543c1d365511b84c34e4e259c5dfb10c
L-Spiro <yogurtemperor@gmail.com>
2024-11-27 08:37:03 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NeuralNet9
+ An industrial-scale deep-learning library intended to be as scalable as TensorFlow while being faster and easier to use, and more scalable than PyTorch while being just as easy to use and as fast or faster.
---
+++ Src/NeuralNet9.cpp
@@ -0,0 +1,9 @@
+// NeuralNet9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int wmain( int _iArgC, wchar_t const * _wcpArgV[] ) {
+    --_iArgC;
+    std::cout << "Hello World!\n";
+}
